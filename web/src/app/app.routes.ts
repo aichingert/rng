43aefdba7c4c72525c7c79866b0db67c159ac363
aichingert <aichinger.tobias13@gmail.com>
2024-03-07 15:25:52 +0100

    feat(web): login and register implemented

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import {GameComponent} from "./game/game.component";
 import {LobbyComponent} from "./lobby/lobby.component";
 import {LoginComponent} from "./login/login.component";
-import {AuthService} from "../shared/auth.service";
+import {RegisterComponent} from "./register/register.component";
 
 export const routes: Routes = [
   { path: "lobby", component: LobbyComponent },
   { path: "login", component: LoginComponent },
-  { path: "play", component: GameComponent, canActivate: [AuthService] },
+  { path: "register", component: RegisterComponent },
+  { path: "play", component: GameComponent },
   { path: "", redirectTo: "lobby", pathMatch: "full" }
 ];
--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {LobbyService} from "../../shared/lobby.service";
+import {FormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {UserService} from "../../shared/user.service";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatError, MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatError,
+    MatFormField,
+    MatInput
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   public username: string = "";
+  public password: string = "";
+  public invalidLogin: boolean = false;
 
-  constructor (
-    private router: Router,
-    private lobbyService: LobbyService,
-  ) {}
+  private loginSubscription: Subscription = new Subscription();
 
-  public onKey(event: KeyboardEvent): void {
+  constructor (private user: UserService,) {}
+
+  ngOnDestroy() {
+    this.loginSubscription.unsubscribe();
+  }
+
+  onSubmit(_event: Event): void {
+    this.loginSubscription = this.user
+      .login(this.username, this.password)
+      .subscribe((_) => this.invalidLogin = true);
   }
 }
