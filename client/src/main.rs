4f2790ee734acc10f4f10deaebb2401c0d4e75e8
aichingert <aichinger.tobias13@gmail.com>
2024-06-03 22:49:35 +0200

    feat(client): started drawing grid

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,7 @@
 use iced::*;
 use iced::widget::{
     container, column,
-    canvas::{self, Event, Canvas, Frame, Geometry}
+    canvas::{self, Event, Canvas, Cache, Frame, Geometry, Path, Stroke}
 };
 
 pub fn main() -> iced::Result{
@@ -31,6 +31,7 @@ enum Field {
 #[derive(Default)]
 struct Grid {
     pub fields: Vec<Field>,
+    pub cache: Cache,
 }
 
 impl Grid {
@@ -47,6 +48,8 @@ enum Interaction {
     #[default]
     N 
 }
+
+const CELL_SIZE: u16 = 50;
 
 impl canvas::Program<Message> for Grid {
     type State = Interaction;
@@ -69,7 +72,34 @@ impl canvas::Program<Message> for Grid {
         bounds: Rectangle,
         cursor: mouse::Cursor,
     ) -> Vec<Geometry> {
-        vec![]
+        let (w, h) = (bounds.width, bounds.height);
+
+        let grid = self.cache.draw(renderer, bounds.size(), |frame| {
+            let background = Path::rectangle(Point::ORIGIN, frame.size());
+            frame.fill(&background, Color::from_rgb8(0x40, 0x44, 0x4B));
+
+            frame.with_save(|frame| {
+                frame.scale(CELL_SIZE);
+
+                let x = (w / 3.) / (CELL_SIZE as f32);
+                let y = (h / 2.) / (CELL_SIZE as f32);
+
+                for i in 1..=2 {
+                    let hor_src = Point::new((i as f32) * x, y - (y / 1.5));
+                    let hor_dst = Point::new((i as f32) * x, y + (y / 1.5));
+
+
+                    let ver_src = Point::new(y - (y / 1.5), (i as f32) * x);
+                    let ver_dst = Point::new(2.5 * y, (i as f32) * x);
+
+                    frame.stroke(&Path::line(hor_src, hor_dst), Stroke::default().with_color(Color::new(1., 1., 1., 1.)));
+                    frame.stroke(&Path::line(ver_src, ver_dst), Stroke::default().with_color(Color::new(1., 1., 1., 1.)));
+                }
+
+            });
+        });
+
+        vec![grid]
     }
 
     fn mouse_interaction(
@@ -94,7 +124,7 @@ impl Application for Toc {
     fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
         (
             Self {
-                grid: Grid { fields: vec![] }
+                grid: Grid { fields: vec![], cache: Cache::new() }
             },
             Command::none(),
         )
