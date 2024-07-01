5a5c643d459ab788ea87b69863926131f5546bbc
aichingert <aichinger.tobias13@gmail.com>
2024-07-01 23:07:56 +0200

    chore: removed grpc-web

--- web/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {GameComponent} from "./game/game.component";
-import {LobbyComponent} from "./lobby/lobby.component";
-
-export const routes: Routes = [
-  { path: "lobby", component: LobbyComponent },
-  { path: "play", component: GameComponent },
-  { path: "", redirectTo: "lobby", pathMatch: "full" }
-];
