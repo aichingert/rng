764c9d00758eb04b6932b25a16d1b8df33acf2c1
aichingert <aichinger.tobias13@gmail.com>
2024-07-09 21:15:07 +0200

    feat: added app with raylib as gui library

--- capnp/run.sh
+++ capnp/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-zig c++ -std=c++14 -Wall server.cpp hello.capnp.cpp \
+zig c++ -std=c++20 -Wall server.cpp hello.capnp.cpp \
     $(pkg-config --cflags --libs capnp-rpc) -o server
 
-zig c++ -std=c++14 -Wall client.cpp hello.capnp.cpp \
+zig c++ -std=c++20 -Wall client.cpp hello.capnp.cpp \
     $(pkg-config --cflags --libs capnp-rpc) -o client
