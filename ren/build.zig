61ea079ab2cd5cc5f928a65ff0d7138596b03000
aichingert <aichinger.tobias13@gmail.com>
2024-08-08 21:06:26 +0200

    feat: setup glfw for builds other than wasm

--- ren/build.zig
+++ ren/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub const Platform = enum {
     web,
-    unix,
+    linux,
     win32,
     macos,
 
@@ -10,7 +10,7 @@ pub const Platform = enum {
         if (target.cpu.arch == .wasm32) return .web;
         if (target.os.tag == .windows) return .win32;
         if (target.os.tag == .macos) return .macos;
-        return .unix;
+        return .linux;
     }
 };
 
@@ -18,39 +18,147 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const platform = b.option(Platform, "platform", "ren platform to use") orelse Platform.fromTarget(target.result);
+    const module = b.addModule("ren", .{
+        .root_source_file = b.path("src/main.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const platform = b.option(
+        Platform,
+        "platform",
+        "ren platform to use",
+    ) orelse Platform.fromTarget(target.result);
 
     const build_options = b.addOptions();
     build_options.addOption(Platform, "platform", platform);
 
-    const exe = b.addExecutable(.{
-        .name = "ren",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("options", build_options.createModule());
+    module.addImport("options", build_options.createModule());
 
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+    switch (platform) {
+        .linux, .win32, .macos => {
+            setupGlfw(b, module, platform);
+        },
+        else => {},
     }
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
+    const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    var iter = module.import_table.iterator();
+    while (iter.next()) |e| {
+        unit_tests.root_module.addImport(e.key_ptr.*, e.value_ptr.*);
+    }
+    setupGlfw(b, &unit_tests.root_module, platform);
 
+    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
+}
+
+pub fn setupGlfw(b: *std.Build, module: *std.Build.Module, platform: Platform) void {
+    const target = module.resolved_target orelse b.host;
+
+    const glfw = b.dependency("glfw", .{
+        .target = target,
+        .optimize = module.optimize.?,
+    });
+
+    const libGlfw = b.addStaticLibrary(.{
+        .name = "glfw",
+        .target = target,
+        .optimize = module.optimize.?,
+    });
+
+    libGlfw.linkLibC();
+    libGlfw.addIncludePath(glfw.path("include"));
+
+    switch (platform) {
+        .win32 => {
+            libGlfw.addCSourceFiles(.{
+                .root = .{
+                    .dependency = .{
+                        .dependency = glfw,
+                        .sub_path = "src",
+                    },
+                },
+                .files = &.{
+                    "platform.c",
+                    "monitor.c",
+                    "init.c",
+                    "vulkan.c",
+                    "input.c",
+                    "context.c",
+                    "window.c",
+                    "osmesa_context.c",
+                    "egl_context.c",
+                    "null_init.c",
+                    "null_monitor.c",
+                    "null_window.c",
+                    "null_joystick.c",
+                    "wgl_context.c",
+                    "win32_thread.c",
+                    "win32_init.c",
+                    "win32_monitor.c",
+                    "win32_time.c",
+                    "win32_joystick.c",
+                    "win32_window.c",
+                    "win32_module.c",
+                },
+                .flags = &.{"-D_GLFW_WIN32"},
+            });
+        },
+        .linux => {
+            libGlfw.addCSourceFiles(.{
+                .root = .{
+                    .dependency = .{
+                        .dependency = glfw,
+                        .sub_path = "src",
+                    },
+                },
+                .files = &.{
+                    "platform.c",
+                    "monitor.c",
+                    "init.c",
+                    "vulkan.c",
+                    "input.c",
+                    "context.c",
+                    "window.c",
+                    "osmesa_context.c",
+                    "egl_context.c",
+                    "null_init.c",
+                    "null_monitor.c",
+                    "null_window.c",
+                    "null_joystick.c",
+                    "posix_time.c",
+                    "posix_thread.c",
+                    "posix_module.c",
+                    "egl_context.c",
+                    "xkb_unicode.c",
+                    "linux_joystick.c",
+                    "posix_poll.c",
+
+                    // FIXME: only supports X11 right now
+
+                    "x11_init.c",
+                    "x11_monitor.c",
+                    "x11_window.c",
+                    "glx_context.c",
+                },
+                .flags = &.{},
+            });
+
+            libGlfw.defineCMacro("_GLFW_X11", "1");
+            libGlfw.linkSystemLibrary("X11");
+        },
+        .macos => @panic("not yet supported"),
+        else => @panic("wasm has the browser"),
+    }
+
+    libGlfw.installHeader(glfw.path("include/GLFW/glfw3.h"), "GLFW/glfw3.h");
+
+    module.linkLibrary(libGlfw);
 }
--- ren/src/main.zig
+++ ren/src/main.zig
@@ -3,7 +3,31 @@ const options = @import("options");
 
 const Graphics = @import("Graphics.zig");
 
+const glfw = @cImport({
+    @cInclude("GLFW/glfw3.h");
+});
+
 pub fn main() !void {
-    var graphics = Graphics.init();
-    graphics.increment();
+    if (glfw.glfwInit() == 0) {
+        std.debug.print("Oh no\n", .{});
+    }
+
+    const monitor = glfw.glfwGetPrimaryMonitor();
+    const window = glfw.glfwCreateWindow(640, 480, "Ren", null, null);
+
+    const mode = glfw.glfwGetVideoMode(monitor);
+
+    glfw.glfwWindowHint(glfw.GLFW_RED_BITS, mode.*.redBits);
+    glfw.glfwWindowHint(glfw.GLFW_GREEN_BITS, mode.*.greenBits);
+    glfw.glfwWindowHint(glfw.GLFW_BLUE_BITS, mode.*.blueBits);
+
+    //while (glfw.glfwWindowShouldClose(window) == 0) {}
+
+    glfw.glfwDestroyWindow(window);
+    glfw.glfwTerminate();
+}
+
+test "setup glfw" {
+    try std.testing.expect(glfw.glfwInit() != 0);
+    glfw.glfwTerminate();
 }
